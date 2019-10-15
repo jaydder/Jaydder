6d552f653c20282cb86fef269098ad0dc6a8edbb
jaydder <jaydderfavoravel@gmail.com>
2019-10-15 19:52:54 -0300

    Javascript

--- javaScript/cursoRocket/estrutura_do_app/todos.js
+++ javaScript/cursoRocket/estrutura_do_app/todos.js
@@ -4,18 +4,47 @@ var buttonElement = document.querySelector('#app button');
 
 var todos = [
     'Fazer café',
-    'Estudar Jabascript',
+    'Estudar Javascript',
     'Acessar Comunidade'
 ];
 
 function renderTodos() {
+    listElement.innerHTML = '';
     for (todo of todos){
         var todoElement = document.createElement('li');
         var todoText = document.createTextNode(todo);
-            
+
+        var linkElement = document.createElement('a');
+        linkElement.setAttribute('href','#');   
+
+        var pos = todos.indexOf(todo);
+        linkElement.setAttribute('onclick','deleteTodo('+ pos +')');
+
+        var linkText = document.createTextNode(' Excluir');
+
+        linkElement.appendChild(linkText);
+
         todoElement.appendChild(todoText);
-        listElement.appendChild(todoElement)
+        todoElement.appendChild(linkElement);
+        
+        listElement.appendChild(todoElement);
+        
     }
 }
 
 renderTodos();
+
+function addTodo() { 
+    var todoText = inputElement.value;
+
+    todos.push(todoText);
+    inputElement.value = '';
+    renderTodos();
+};
+
+buttonElement.onclick = addTodo();
+
+function deleteTodo(pos) {
+    todos.splice(pos, 1);
+    renderTodos();
+}
